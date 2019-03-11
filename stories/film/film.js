951005953ce5877e6241eeacafd908167cf1afcf
Irina Ageenko <ira.ageenkooo@gmail.com>
2019-03-12 01:08:01 +0300

    some shit in pr

--- stories/film/film.js
+++ stories/film/film.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import cn from 'classnames';
+import Rodal from 'rodal';
+import 'rodal/lib/rodal.css';
+
+// import Img from 'react-image';
+// import imgage from './sofa.png';
+import './film.css';
 
 const Div = styled.div `
-    &.wrapper{
-        float: left;
+    &.film-wrapper {
+        
+        z-index: 1;
+        transition: 0.6s;
+    }
+    &.wrapper {
         margin:1.7vw;
+        perspective: 1000;
+        transition: 0.6s;
+        cursor: pointer;
+        > Div {
+            height: 23vw;
+	        width: 15vw;
+            transition: 0.6s;
+            transform-style: preserve-3d;
+        }
+    }
+    &.opened {
+        transform: rotateY(180deg) scale(2);
+        margin-top: 50%;
+        position: absolute;
+        margin-left: 50%;
+        
     }
     &.film{
+        z-index: 2;
+        backface-visibility: hidden;
+        top: 0;
+        left: 0;
         border: 0.5px solid white;
         height: 23vw;
 	    width: 15vw;
@@ -22,7 +51,7 @@ const Div = styled.div `
         overflow: hidden;
         box-shadow: 10px 11px 25px -4px rgba(0,0,0,0.75);
 	}
-	
+
     &.poster{
 	    height: 75%;
 	    width: 100%;
@@ -44,14 +73,76 @@ const P = styled.p `
 class Film extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { visible: false };
+        this.getPos = this.getPos.bind(this);
     }
+
+    getPos() {
+        let top = document.getElementsByClassName(this.props.src)[0].getBoundingClientRect().top;
+        let left = document.getElementsByClassName(this.props.src)[0].getBoundingClientRect().left;
+        let modal = document.getElementsByClassName(this.props.src)[1].querySelector(".rodal-dialog");
+        modal.style.top = top+"px";
+        modal.style.left = left+"px";
+        modal.style.bottom = "auto";
+        modal.style.right = "auto";
+            var style = document.createElement('style');
+            style.type = 'text/css';
+            var keyFrames = '\
+            @-webkit-keyframes loader {\
+                0% {\
+                    top: top_DYNAMIC_VALUE;\
+                    bottom: bottom_DYNAMIC_VALUE;\
+                    right: right_DYNAMIC_VALUE;\
+                    left: left_DYNAMIC_VALUE;\
+                }\
+                100% {\
+                    top: 0;\
+                    bottom: 0;\
+                    right: 0;\
+                    left: 0;\
+                }\
+            }\
+            @-moz-keyframes loader {\
+                0% {\
+                    top: top_DYNAMIC_VALUE;\
+                    bottom: bottom_DYNAMIC_VALUE;\
+                    right: right_DYNAMIC_VALUE;\
+                    left: left_DYNAMIC_VALUE;\
+                }\
+                100% {\
+                    top: 0;\
+                    bottom: 0;\
+                    right: 0;\
+                    left: 0;\
+                }\
+            }';
+            style.innerHTML = keyFrames.replace(/top_DYNAMIC_VALUE/g, top+"px").replace(/bottom_DYNAMIC_VALUE/g, "0px").replace(/right_DYNAMIC_VALUE/g, "0px").replace(/left_DYNAMIC_VALUE/g, left+"px");
+            document.getElementsByTagName('head')[0].appendChild(style);
+            document.head.appendChild(style)
+
+    }
+
+    show() {
+        this.getPos()
+        this.setState({ visible: true });
+    }
+
+    hide() {
+        this.setState({ visible: false });
+    }
+
     render() {
         return (
-            <Div className={"wrapper"} onClick = {this.props.onClick} >
-                <Div className="film" style={{backgroundImage:'url(' + this.props.src + ')'}}>
+            <div>
+                <Div className={"wrapper"} onClick={this.show.bind(this)} >
+                <Div className={this.props.src + " film"} style={{backgroundImage:'url(' + this.props.src + ')'}}>
                     <P>{this.props.name}</P>
                 </Div>
             </Div>
+            <Rodal visible={this.state.visible} className={this.props.src + " film"} animation="flip" onClose={this.hide.bind(this)}>
+                <div>Content</div>
+            </Rodal>
+            </div>
         )
     }
 }
--- stories/index.js
+++ stories/index.js
@@ -4,7 +4,7 @@ import { Button } from '@storybook/react/demo';
 import Film from './film/film.js';
 import FilmsTable from './filmsTable/filmsTable.js';
 
-const myItems = [{ name: 'item 1' }, { name: 'item2' }, { name: 'item3'}, { name: 'item 1' }, { name: 'item2' }, { name: 'item3'}];
+const myItems = [{ name: 'item 1', src: "https://cdn1-www.comingsoon.net/assets/uploads/2017/04/rampagefilming.jpg" }, { name: 'item2' , src: "https://upload.wikimedia.org/wikipedia/ru/thumb/9/9e/%D0%9B%D1%91%D0%B4_%28%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%2C_2018%29.jpg/211px-%D0%9B%D1%91%D0%B4_%28%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%2C_2018%29.jpg"}];
 
 storiesOf('Button', module)
   .add('with text', () => (
